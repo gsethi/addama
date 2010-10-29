5810377d2b668e25298541dcd24cf5fa56d52501
nicole deflaux <nicole.deflaux@sagebase.org>
2010-10-29 17:42:52 +0000

    Wrote unit tests for JcrSearchController in preparation for enabling search over non-text fields.  I also refactored the test JCR support into more reusable code and spring configuration.  Lastly I changed the repo to be a TransientRepository so that the repo is clean and empty at the start of each test.
    new review:
    owner: hrovira.isb
    cc: aeakin@systemsbiology.org, mike.kellen@sagebionetworks.com
    summary: Please review search unit tests
    labels: ReadyForReview
    branch: /branches/deflaux/playground
    Please review the search unit tests.  Specifically take a look at the TODOs in
    the file and let me know whether modifying search to support those cases is an okay extension or will break the semantics you are going for with your search strategy.

--- playground/local-svcs/jcr-repositories-svc/src/test/java/org/systemsbiology/addama/rest/transforms/CreateOrUpdateNodeTest.java
+++ playground/local-svcs/jcr-repositories-svc/src/test/java/org/systemsbiology/addama/rest/transforms/CreateOrUpdateNodeTest.java
@@ -5,11 +5,7 @@ import static org.junit.Assert.assertTrue;
 import static org.systemsbiology.addama.rest.AddamaKeywords.created_at;
 import static org.systemsbiology.addama.rest.AddamaKeywords.last_modified_at;
 
-import java.util.logging.Logger;
-
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
-import javax.jcr.Session;
 
 import org.json.JSONObject;
 import org.junit.After;
@@ -19,96 +15,84 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springmodules.jcr.JcrTemplate;
-import org.springmodules.jcr.SessionFactory;
-import org.springmodules.jcr.SessionFactoryUtils;
+import org.systemsbiology.addama.JcrTestHelper;
 import org.systemsbiology.addama.jcr.callbacks.GetNodeAtPathJcrCallback;
-import org.systemsbiology.addama.rest.JcrCrudController;
 
 /**
- * This test suite makes use of an in-memory JCR.  Note that the JCR
- * lives for the duration of the suite, so subsequent tests will see
- * changes to the JCR made by prior tests.  For test isolation, merely
- * place the tests in separate files.
- *
- * See
- * src/test/resources/org/systemsbiology/addama/rest/transforms/CreateOrUpdateNodeTest-context.xml
- * for spring configuration specific to this test.
- *
- * See src/test/resources/jackrabbit-testrepo.xml for spring
- * configuration for an in-memory JCR that can be used by any test
- * suite
- *
+ * This test suite was created to add a test for a bug where the last modified
+ * field of an Addama node was not getting set upon update.
+ * 
+ * See src/test/resources/org/systemsbiology/addama/rest/transforms/
+ * CreateOrUpdateNodeTest-context.xml for spring configuration specific to this
+ * test.
+ * 
+ * See src/test/resources/jackrabbit-testrepo.xml for spring configuration for
+ * an in-memory JCR that can be used by any test suite
+ * 
  * @author deflaux
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class CreateOrUpdateNodeTest {
-    private static final Logger log =
-        Logger.getLogger(CreateOrUpdateNodeTest.class.getName());
-    private SessionFactory sf;
-    private Session session;
 
-    @Autowired
-    private JcrTemplate jcrTemplate = null;
+	@Autowired
+	private JcrTestHelper helper = null;
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        // Ensure that the JCR session remains open for the duration
-        // of the test case
-        sf = jcrTemplate.getSessionFactory();
-        session = SessionFactoryUtils.getSession(sf, true);
-        TransactionSynchronizationManager.bindResource(sf,
-                sf.getSessionHolder(session));
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		// Ensure that the JCR session remains open for the duration
+		// of the test case
+		helper.obtainSession();
+	}
 
-        // TODO: figure out how to check whether the JCR repo is
-        // "empty" and perform this check before the first test case.
-        // We assume none would edit the test spring configuration to
-        // point to a production JCR, but it never hurts to have a
-        // healthy amount of paranoia
-    }
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		helper.releaseSession();
+	}
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-        TransactionSynchronizationManager.unbindResource(sf);
-        SessionFactoryUtils.releaseSession(session, sf);
-    }
+	/**
+	 * Test method for
+	 * {@link org.systemsbiology.addama.rest.transforms.CreateOrUpdateNode#doCreate(javax.jcr.Node, org.json.JSONObject)}
+	 * .
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testLastModified() throws Exception {
 
-    /**
-     * Test method for {@link org.systemsbiology.addama.rest.transforms.CreateOrUpdateNode#doCreate(javax.jcr.Node, org.json.JSONObject)}.
-     * @throws Exception
-     */
-    @Test
-    public void testLastModified() throws Exception {
+		// Dev note: this could be refactored to use
+		// hellper.createOrUpdateNode(path, json) but you really want to see the
+		// API being tested in the actual unit test code so I did not do that.
 
-        Node node = (Node) jcrTemplate.execute(new GetNodeAtPathJcrCallback("/"));
+		JcrTemplate jcrTemplate = helper.getJcrTemplate();
+		Node node = (Node) jcrTemplate.execute(new GetNodeAtPathJcrCallback("/"));
 
-        JSONObject jsonCreate = new JSONObject();
-        jsonCreate.put("uri", "/addama/applications/edgelimma");
-        jsonCreate.put("proxy", "http://jefferson:9081/cgi-bin");
-        jsonCreate.put("excludeBaseUri", true);
+		JSONObject jsonCreate = new JSONObject();
+		jsonCreate.put("uri", "/addama/applications/edgelimma");
+		jsonCreate.put("proxy", "http://jefferson:9081/cgi-bin");
+		jsonCreate.put("excludeBaseUri", true);
 
-        CreateOrUpdateNode createNode = new CreateOrUpdateNode();
-        createNode.doCreate(node, jsonCreate);
-        jcrTemplate.save();
+		CreateOrUpdateNode createNode = new CreateOrUpdateNode();
+		createNode.doCreate(node, jsonCreate);
+		jcrTemplate.save();
 
-        assertTrue(node.hasProperty(created_at.word()));
-        assertFalse(node.hasProperty(last_modified_at.word()));
+		assertTrue(node.hasProperty(created_at.word()));
+		assertFalse(node.hasProperty(last_modified_at.word()));
 
-        JSONObject jsonUpdate = new JSONObject();
-        jsonUpdate.put("foo", "bar");
+		JSONObject jsonUpdate = new JSONObject();
+		jsonUpdate.put("foo", "bar");
 
-        createNode.doCreate(node, jsonUpdate);
-        jcrTemplate.save();
+		createNode.doCreate(node, jsonUpdate);
+		jcrTemplate.save();
 
-        assertTrue(node.hasProperty(created_at.word()));
-        assertTrue(node.hasProperty(last_modified_at.word()));
-    }
+		assertTrue(node.hasProperty(created_at.word()));
+		assertTrue(node.hasProperty(last_modified_at.word()));
+	}
 }
