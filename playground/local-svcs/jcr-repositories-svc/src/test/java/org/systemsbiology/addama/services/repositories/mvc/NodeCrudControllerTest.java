d1a7d42a41e4babb505d11926f88c9134beb4406
nicole deflaux <nicole.deflaux@sagebase.org>
2010-11-16 04:23:22 +0000

    Converting from string tests to JSONObject test per feedback on revision 304.

--- playground/local-svcs/jcr-repositories-svc/src/test/java/org/systemsbiology/addama/services/repositories/mvc/NodeCrudControllerTest.java
+++ playground/local-svcs/jcr-repositories-svc/src/test/java/org/systemsbiology/addama/services/repositories/mvc/NodeCrudControllerTest.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -125,23 +126,42 @@ public class NodeCrudControllerTest {
 		servlet.service(request, response);
 		log.info("Results: " + response.getContentAsString());
 
-		assertEquals(200, response.getStatus());
-		assertTrue(response.getContentAsString().matches(
-				".*unit tests are helpful.*"));
-		assertTrue(response.getContentAsString().matches(
-				"\\{\"aBoolean\":false,"
-				+ "\"numberOfItems\":0,"
-				+ "\"items\":\\[\\],"
-				+ "\"isFile\":false,"
-				+ "\"created-at\":\"\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}\","
-				+ "\"anInteger\":-10,"
-				+ "\"freeText\":\"unit tests are helpful\","
-				+ "\"aSingleWord\":\"same\","
-				+ "\"operations\":\\{\"directory\":\"/path/TestData/TestNode3/dir\","
-				+ "\"terms\":\"/path/TestData/TestNode3/annotations/terms\","
-				+ "\"annotations\":\"/path/TestData/TestNode3/annotations\","
-				+ "\"meta\":\"/path/TestData/TestNode3/meta\"\\},"
-				+ "\"uri\":\"/path/TestData/TestNode3\"\\}"));
+		assertEquals("we got 200 OK", 200, response.getStatus());
+		assertTrue("we got the node we expected",
+				response.getContentAsString().matches(".*unit tests are helpful.*"));
+		
+		// Now check the structure of our response, it should look something like:
+		// {
+		//  "aBoolean":false,
+		//  "numberOfItems":0,
+		//  "items":[],
+		//  "isFile":false,
+		//  "created-at":"11/15/2010 19:52:34",
+		//  "anInteger":-10,
+		//  "freeText":"unit tests are helpful",
+		//  "aSingleWord":"same",
+		//  "operations":{
+		//     "directory":"/path/TestData/TestNode3/dir",
+		//     "terms":"/path/TestData/TestNode3/annotations/terms",
+		//     "annotations":"/path/TestData/TestNode3/annotations",
+		//     "meta":"/path/TestData/TestNode3/meta"},
+		//  "uri":"/path/TestData/TestNode3"
+		//}
+
+		JSONObject results = new JSONObject(response.getContentAsString());
+		assertEquals(false, results.getBoolean("aBoolean"));
+		assertEquals(0, results.getInt("numberOfItems"));
+		assertEquals(0, results.getJSONArray("items").length());
+		assertEquals(false, results.getBoolean("isFile"));
+		assertTrue(results.getString("created-at").matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}"));
+		assertEquals(-10, results.getInt("anInteger"));
+		assertEquals("unit tests are helpful", results.getString("freeText"));
+		assertEquals("same", results.getString("aSingleWord"));
+		assertEquals("/path/TestData/TestNode3/dir", results.getJSONObject("operations").getString("directory"));
+		assertEquals("/path/TestData/TestNode3/annotations", results.getJSONObject("operations").getString("annotations"));
+		assertEquals("/path/TestData/TestNode3/annotations/terms", results.getJSONObject("operations").getString("terms"));
+		assertEquals("/path/TestData/TestNode3/meta", results.getJSONObject("operations").getString("meta"));
+		assertEquals("/path/TestData/TestNode3", results.getString("uri"));
 	}
 
 	/**
@@ -153,68 +173,120 @@ public class NodeCrudControllerTest {
 	 */
 	@Test
 	public final void testPost() throws Exception {
+		/**	
+		 * Create a node
+		 */
 		request.setMethod("POST");
 		request.setRequestURI("/path/TestData/TestNode4");
 		request.setParameter("JSON",
 				"{\"foobar\":\"this is a newly created node\"}");
 		servlet.service(request, response);
 		log.info("Results: " + response.getContentAsString());
-		assertEquals(200, response.getStatus());
-		assertTrue(response.getContentAsString().matches(
-				".*this is a newly created node.*"));
-		assertTrue(response.getContentAsString().matches(
-				"\\{\"numberOfItems\":0,"
-				+ "\"foobar\":\"this is a newly created node\","
-				+ "\"items\":\\[\\],"
-				+ "\"isFile\":false,"
-				+ "\"created-at\":\"\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}\","
-				+ "\"operations\":\\{\"directory\":\"/path/TestData/TestNode4/dir\","
-				+ "\"terms\":\"/path/TestData/TestNode4/annotations/terms\","
-				+ "\"annotations\":\"/path/TestData/TestNode4/annotations\","
-				+ "\"meta\":\"/path/TestData/TestNode4/meta\"\\},"
-				+ "\"uri\":\"/path/TestData/TestNode4\"\\}"));
-
+		assertEquals("we got 200 OK", 200, response.getStatus());
+		assertTrue("we got our newly created node",
+				response.getContentAsString().matches(".*this is a newly created node.*"));
+		// Now check the structure of our response, it should look something like:
+		// {
+		//  "numberOfItems":0,
+		//  "foobar":"this is a newly created node",
+		//  "items":[],
+		//  "isFile":false,
+		//  "created-at":"11/15/2010 19:58:54",
+		//  "operations":{
+		//     "directory":"/path/TestData/TestNode4/dir",
+		//     "terms":"/path/TestData/TestNode4/annotations/terms",
+		//     "annotations":"/path/TestData/TestNode4/annotations",
+		//     "meta":"/path/TestData/TestNode4/meta"},
+		//  "uri":"/path/TestData/TestNode4"
+		// }
+		JSONObject results = new JSONObject(response.getContentAsString());
+		assertEquals(0, results.getInt("numberOfItems"));
+		assertEquals(0, results.getJSONArray("items").length());
+		assertEquals(false, results.getBoolean("isFile"));
+		assertTrue(results.getString("created-at").matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}"));
+		assertEquals("this is a newly created node", results.getString("foobar"));
+		assertEquals("/path/TestData/TestNode4/dir", results.getJSONObject("operations").getString("directory"));
+		assertEquals("/path/TestData/TestNode4/annotations", results.getJSONObject("operations").getString("annotations"));
+		assertEquals("/path/TestData/TestNode4/annotations/terms", results.getJSONObject("operations").getString("terms"));
+		assertEquals("/path/TestData/TestNode4/meta", results.getJSONObject("operations").getString("meta"));
+		assertEquals("/path/TestData/TestNode4", results.getString("uri"));
+		
+		/**
+		 * Modify a node
+		 */
 		response = new MockHttpServletResponse();
 		request.setParameter("JSON",
 		        "{\"foobar\":\"this is a modified node\"}");
 		servlet.service(request, response);
 		log.info("Results: " + response.getContentAsString());
-		assertEquals(200, response.getStatus());
-		assertTrue(response.getContentAsString().matches(
-				".*this is a modified node.*"));
-		assertTrue(response.getContentAsString().matches(
-				"\\{\"numberOfItems\":0,"
-				+ "\"foobar\":\"this is a modified node\","
-				+ "\"items\":\\[\\],"
-				+ "\"isFile\":false,"
-				+ "\"created-at\":\"\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}\","
-				+ "\"operations\":\\{\"directory\":\"/path/TestData/TestNode4/dir\","
-				+ "\"terms\":\"/path/TestData/TestNode4/annotations/terms\","
-				+ "\"annotations\":\"/path/TestData/TestNode4/annotations\","
-				+ "\"meta\":\"/path/TestData/TestNode4/meta\"\\},"
-				+ "\"uri\":\"/path/TestData/TestNode4\","
-				+ "\"last-modified-at\":\"\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}\"\\}"));
+		assertEquals("we got 200 OK", 200, response.getStatus());
+		assertTrue("we got our modified node",
+				response.getContentAsString().matches(".*this is a modified node.*"));
+		// Now check the structure of our response, it should look something like:
+		// {
+		//  "numberOfItems":0,
+		//  "foobar":"this is a modified node",
+		//  "items":[],
+		//  "isFile":false,
+		//  "created-at":"11/15/2010 19:58:54",
+		//  "last-modified-at":"11/15/2010 19:59:24",
+		//  "operations":{
+		//     "directory":"/path/TestData/TestNode4/dir",
+		//     "terms":"/path/TestData/TestNode4/annotations/terms",
+		//     "annotations":"/path/TestData/TestNode4/annotations",
+		//     "meta":"/path/TestData/TestNode4/meta"},
+		//  "uri":"/path/TestData/TestNode4"
+		// }
+		results = new JSONObject(response.getContentAsString());
+		assertEquals(0, results.getInt("numberOfItems"));
+		assertEquals(0, results.getJSONArray("items").length());
+		assertEquals(false, results.getBoolean("isFile"));
+		assertTrue(results.getString("created-at").matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}"));
+		assertTrue(results.getString("last-modified-at").matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}"));
+		assertEquals("this is a modified node", results.getString("foobar"));
+		assertEquals("/path/TestData/TestNode4/dir", results.getJSONObject("operations").getString("directory"));
+		assertEquals("/path/TestData/TestNode4/annotations", results.getJSONObject("operations").getString("annotations"));
+		assertEquals("/path/TestData/TestNode4/annotations/terms", results.getJSONObject("operations").getString("terms"));
+		assertEquals("/path/TestData/TestNode4/meta", results.getJSONObject("operations").getString("meta"));
+		assertEquals("/path/TestData/TestNode4", results.getString("uri"));
 
+		/**
+		 * Create a child node
+		 */
 		response = new MockHttpServletResponse();
 		request.setRequestURI("/path/TestData/TestNode4/ChildA");
 		request.setParameter("JSON",
 				"{\"foobar\":\"this is a newly created child node\"}");
 		servlet.service(request, response);
 		log.info("Results: " + response.getContentAsString());
-		assertEquals(200, response.getStatus());
-		assertTrue(response.getContentAsString().matches(
-				".*this is a newly created child node.*"));
-		assertTrue(response.getContentAsString().matches(
-				"\\{\"numberOfItems\":0,"
-				+ "\"foobar\":\"this is a newly created child node\","
-				+ "\"items\":\\[\\],"
-				+ "\"isFile\":false,"
-				+ "\"created-at\":\"\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}\","
-				+ "\"operations\":\\{\"directory\":\"/path/TestData/TestNode4/ChildA/dir\","
-				+ "\"terms\":\"/path/TestData/TestNode4/ChildA/annotations/terms\","
-				+ "\"annotations\":\"/path/TestData/TestNode4/ChildA/annotations\","
-				+ "\"meta\":\"/path/TestData/TestNode4/ChildA/meta\"\\},"
-				+ "\"uri\":\"/path/TestData/TestNode4/ChildA\"\\}"));
+		assertEquals("we got 200 OK", 200, response.getStatus());
+		assertTrue("we got our child node",
+				response.getContentAsString().matches(".*this is a newly created child node.*"));
+		// Now check the structure of our response, it should look something like:
+		// {
+		//  "numberOfItems":0,
+		//  "foobar":"this is a newly created child node",
+		//  "items":[],
+		//  "isFile":false,
+		//  "created-at":"11/15/2010 19:58:54",
+		//  "operations":{
+		//     "directory":"/path/TestData/TestNode4/ChildA/dir",
+		//     "terms":"/path/TestData/TestNode4/ChildA/annotations/terms",
+		//     "annotations":"/path/TestData/TestNode4/ChildA/annotations",
+		//     "meta":"/path/TestData/TestNode4/ChildA/meta"},
+		//  "uri":"/path/TestData/TestNode4/ChildA"
+		// }
+		results = new JSONObject(response.getContentAsString());
+		assertEquals(0, results.getInt("numberOfItems"));
+		assertEquals(0, results.getJSONArray("items").length());
+		assertEquals(false, results.getBoolean("isFile"));
+		assertTrue(results.getString("created-at").matches("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}"));
+		assertEquals("this is a newly created child node", results.getString("foobar"));
+		assertEquals("/path/TestData/TestNode4/ChildA/dir", results.getJSONObject("operations").getString("directory"));
+		assertEquals("/path/TestData/TestNode4/ChildA/annotations", results.getJSONObject("operations").getString("annotations"));
+		assertEquals("/path/TestData/TestNode4/ChildA/annotations/terms", results.getJSONObject("operations").getString("terms"));
+		assertEquals("/path/TestData/TestNode4/ChildA/meta", results.getJSONObject("operations").getString("meta"));
+		assertEquals("/path/TestData/TestNode4/ChildA", results.getString("uri"));
 	}
 
 	/**
@@ -232,9 +304,9 @@ public class NodeCrudControllerTest {
 		servlet.service(request, response);
 		log.info("Results: " + response.getContentAsString());
 
-		assertEquals(200, response.getStatus());
-		assertEquals("{\"uri\":\"/path/TestData/TestNode3/delete\"}", 
-				response.getContentAsString());
+		assertEquals("we got 200 OK", 200, response.getStatus());
+		JSONObject results = new JSONObject(response.getContentAsString());
+		assertEquals("/path/TestData/TestNode3/delete", results.getString("uri"));
 	}
 
 	/**
@@ -252,8 +324,8 @@ public class NodeCrudControllerTest {
 		servlet.service(request, response);
 		log.info("Results: " + response.getContentAsString());
 
-		assertEquals(200, response.getStatus());
-		assertEquals("{\"uri\":\"/path/TestData/TestNode3\"}", 
-				response.getContentAsString());
+		assertEquals("we got 200 OK", 200, response.getStatus());
+		JSONObject results = new JSONObject(response.getContentAsString());
+		assertEquals("/path/TestData/TestNode3/delete", results.getString("uri"));
 	}
 }
