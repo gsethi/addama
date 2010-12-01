54196cce00a81d12cf5e86676c78ae34812d207c
hrovira isb <hrovira.isb@gmail.com>
2010-12-01 21:23:11 +0000

    cleanup

--- clide-server-api/src/main/java/org/systemsbiology/addama/clide/server/api/ClideServer.java
+++ clide-server-api/src/main/java/org/systemsbiology/addama/clide/server/api/ClideServer.java
@@ -10,18 +10,13 @@ import org.json.JSONObject;
 import org.systemsbiology.addama.commons.httpclient.support.DirectLinkResponseCallback;
 import org.systemsbiology.addama.commons.httpclient.support.HttpClientTemplate;
 import org.systemsbiology.addama.commons.httpclient.support.OkJsonResponseCallback;
-import org.systemsbiology.addama.commons.httpclient.support.StatusCodeCaptureResponseCallback;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.logging.Logger;
 
 /**
  * @author hrovira
  */
 public class ClideServer {
-    private static final Logger log = Logger.getLogger(ClideServer.class.getName());
-
     private HttpClientTemplate httpClientTemplate;
     private String subscribersUri;
 
@@ -37,25 +32,20 @@ public class ClideServer {
         this.subscribersUri = subscribersUri;
     }
 
-    public String[] getClientUris() throws Exception {
-        ArrayList<String> uris = new ArrayList<String>();
+    /*
+     * API Methods
+     */
 
+    public JSONArray getClients() throws Exception {
         JSONObject clients = (JSONObject) httpClientTemplate.executeMethod(new GetMethod(subscribersUri), new OkJsonResponseCallback());
         if (clients != null && clients.has("items")) {
-            JSONArray items = clients.getJSONArray("items");
-            for (int i = 0; i < items.length(); i++) {
-                JSONObject item = items.getJSONObject(i);
-                uris.add(item.getString("uri"));
-            }
+            return clients.getJSONArray("items");
         }
 
-        log.info(uris.toString());
-        return uris.toArray(new String[uris.size()]);
+        return null;
     }
 
-    public void upload(String uri, File... files) throws Exception {
-        log.info(uri);
-
+    public JSONObject push(String uri, File... files) throws Exception {
         Part[] parts = new Part[files.length];
         for (int i = 0; i < files.length; i++) {
             File f = files[i];
@@ -68,11 +58,6 @@ public class ClideServer {
         PostMethod post = new PostMethod(directLink);
         post.setRequestEntity(new MultipartRequestEntity(parts, post.getParams()));
 
-        JSONObject json = (JSONObject)httpClientTemplate.executeMethod(post, new OkJsonResponseCallback());
-        if (json != null) {
-            log.info("response=" + json.toString(4));
-        } else {
-            log.warning("unknown response");
-        }
+        return (JSONObject) httpClientTemplate.executeMethod(post, new OkJsonResponseCallback());
     }
 }
--- clide-server-api/src/main/java/org/systemsbiology/addama/clide/server/api/Main.java
+++ clide-server-api/src/main/java/org/systemsbiology/addama/clide/server/api/Main.java
@@ -1,15 +1,20 @@
 package org.systemsbiology.addama.clide.server.api;
 
 import org.apache.commons.lang.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * @author hrovira
  */
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) throws Exception {
         if (args == null || args.length == 0) {
             throw new IllegalArgumentException("must include at least one file to upload in arguments");
@@ -18,20 +23,33 @@ public class Main {
         ApplicationContext appCtx = new ClassPathXmlApplicationContext(new String[]{"apiKeyHttpClientTemplate.xml", "clideServer.xml"});
 
         ClideServer clideServer = (ClideServer) appCtx.getBean("clideServer");
-        String[] clientUris = clideServer.getClientUris();
-        if (clientUris != null && clientUris.length > 0) {
-            for (String clientUri : clientUris) {
+        JSONArray clients = clideServer.getClients();
+        if (clients != null && clients.length() > 0) {
+            for (int i = 0; i < clients.length(); i++) {
+                JSONObject client = clients.getJSONObject(i);
+                String clientUri = client.getString("uri");
+
                 for (String arg : args) {
                     File f = new File(arg);
+                    log.info("\n\tpushing:" + f.getPath() + "\n\tto:" + clientUri + "\n\n");
                     if (f.exists()) {
                         if (f.isDirectory()) {
-                            clideServer.upload(getUri(clientUri, f), f.listFiles());
+                            push(clideServer, getUri(clientUri, f), f.listFiles());
                         } else {
-                            clideServer.upload(getUri(clientUri, f.getParentFile()), f);
+                            push(clideServer, getUri(clientUri, f.getParentFile()), f);
                         }
                     }
                 }
             }
+        }
+    }
+
+    private static void push(ClideServer clideServer, String uri, File... files) throws Exception {
+        JSONObject response = clideServer.push(uri, files);
+        if (response != null) {
+            log.info("\nresponse=" + response.toString(4) + "\n");
+        } else {
+            log.warning("\ninvalid response\n");
         }
     }
 
