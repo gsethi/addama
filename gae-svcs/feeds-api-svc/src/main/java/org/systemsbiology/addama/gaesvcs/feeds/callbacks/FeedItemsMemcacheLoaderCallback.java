d21a407dc8e74f4a2a58a7250cba1dfb0230491c
nicole deflaux <nicole.deflaux@sagebase.org>
2010-12-07 23:09:03 +0000

    Merging the following feeds-api-svc changes per hrovira.  Code review request was " Issue 24:   Feeds API Service Enhancements"
    ------------------------------------------------------------------------
    r327 | nicole.deflaux@sagebase.org | 2010-11-21 11:29:59 -0800 (Sun, 21 Nov 2010) | 3 lines
    
    The changed query in revision 324 needed an index in the datastore.  Unfortunatley the error regarding the missing index was not thrown via the unit test framework.  The error was thrown by both the production deployed instance and the dev appengine instance.  In the future I need to either test with the dev appengine or find a way to configure the unittests such that they will detect this sort of error.  I ran the dev appengine to autogenerate the contents to be placed into datastore-indexes.xml
    
    
    ------------------------------------------------------------------------
    r324 | nicole.deflaux@sagebase.org | 2010-11-19 17:38:03 -0800 (Fri, 19 Nov 2010) | 17 lines
    
    Feature changes:
     (1) feed items are now returned sorted by date returning most recent first
    
     (2) feed items are now paginated, returning ten feed items per page
    
     (3) feed items can be added idempotently or updated if you pass an optional 'key' parameter in the JSON object
    
     (4) feed items can now set explicit values for 'title' and 'link' (rather than using the default values) if you pass those parameters in the JSON object
    
    Potential areas of concern -> please speak up if these seem to be a problem and I can do something about them:
     (1) the page size is fixed at 10 items, the spec http://tools.ietf.org/html/rfc5005#section-3 does not support a parameter to specify the page size
    
     (2) due to an implementation detail, feed names are now limited to 100 characters in length, if that limit is too short, I can use a digest of the feed name as the memcache key
    
     (3) the date parameter format is now expected in ISO8601 format, Note that Addama seems to use several different formats for dates and it might be good to standardize on a single format.
    
    
    ------------------------------------------------------------------------
    r317 | nicole.deflaux@sagebase.org | 2010-11-18 11:24:47 -0800 (Thu, 18 Nov 2010) | 3 lines
    
    Added happy case tests for the rest of the requests supported by this controller.  Switched to testing at the dispatcher layer to check that the RSS view is correctly rendered.
    
    
    ------------------------------------------------------------------------
    r316 | nicole.deflaux@sagebase.org | 2010-11-17 16:16:36 -0800 (Wed, 17 Nov 2010) | 3 lines
    
    Added a unit test for existing functionality in feeds-api-svn in preparation for extending the functionality offered.

--- gae-svcs/feeds-api-svc/src/main/java/org/systemsbiology/addama/gaesvcs/feeds/callbacks/FeedItemsMemcacheLoaderCallback.java
+++ gae-svcs/feeds-api-svc/src/main/java/org/systemsbiology/addama/gaesvcs/feeds/callbacks/FeedItemsMemcacheLoaderCallback.java
@@ -1,27 +1,49 @@
 package org.systemsbiology.addama.gaesvcs.feeds.callbacks;
 
-import com.google.appengine.api.datastore.*;
-import org.json.JSONObject;
-import org.systemsbiology.addama.commons.gae.dataaccess.MemcacheLoaderCallback;
-
 import java.io.Serializable;
 
+import org.json.JSONObject;
+import org.systemsbiology.addama.commons.gae.dataaccess.MemcachePaginatedLoaderCallback;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+
 /**
+ * Return a paginated set of feed items sorted by date to return the most recent feed items first
+ *  
  * @author hrovira
  */
-public class FeedItemsMemcacheLoaderCallback implements MemcacheLoaderCallback {
-    private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+public class FeedItemsMemcacheLoaderCallback implements MemcachePaginatedLoaderCallback {
+	
+	private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
 
-    public Serializable getCacheableObject(String feedUri) throws Exception {
+
+	public Serializable getCacheableObject(String feedUri, int limit, int offset) throws Exception {
         JSONObject json = new JSONObject();
 
         Key pk = KeyFactory.createKey("feeds", feedUri);
-        PreparedQuery pq = datastoreService.prepare(new Query("feed-item", pk));
-        for (Entity entity : pq.asIterable()) {
+        Query query = new Query("feed-item", pk);
+        query.addSort("date", Query.SortDirection.DESCENDING);
+
+        PreparedQuery pq = datastoreService.prepare(query);
+        for (Entity entity : pq.asIterable(FetchOptions.Builder.withLimit(limit).offset(offset))) {
             JSONObject item = new JSONObject();
             item.put("author", entity.getProperty("author").toString());
             item.put("text", entity.getProperty("text").toString());
             item.put("date", entity.getProperty("date").toString());
+            // Handle optional fields
+            if(entity.hasProperty("title")) {
+                item.put("title", entity.getProperty("title").toString());            	
+            }
+            if(entity.hasProperty("link")) {
+                item.put("link", entity.getProperty("link").toString());            	
+            }
             json.append("items", item);
         }
 
