c23da11a4848a56549d3aa50db3eb420b139ee18
hrovira <hrovira@localhost>
2011-09-01 20:15:51 +0000

    added service to provide websocket eventing

---
+++ local-svcs/ws-eventing-svc/src/main/java/org/systemsbiology/addama/services/eventing/mvc/EventsController.java
@@ -0,0 +1,8 @@
+package org.systemsbiology.addama.services.eventing.mvc;
+
+/**
+ * @author hrovira
+ */
+public class EventsController {
+
+}
---
+++ local-svcs/ws-eventing-svc/src/main/webapp/js/application.js
@@ -0,0 +1,48 @@
+dojo.require("dojox.cometd");
+
+dojo.addOnLoad(function()
+{
+    var _connected = false;
+
+    function _connectionSucceeded()
+    {
+        dojo.byId('body').innerHTML = 'CometD Connection Succeeded';
+    }
+
+    function _connectionBroken()
+    {
+        dojo.byId('body').innerHTML = 'CometD Connection Broken';
+    }
+
+    function _metaConnect(message)
+    {
+        var wasConnected = _connected;
+        _connected = message.successful === true;
+        if (!wasConnected && _connected)
+        {
+            _connectionSucceeded();
+        }
+        else if (wasConnected && !_connected)
+        {
+            _connectionBroken();
+        }
+    }
+
+    var cometd = dojox.cometd;
+
+    // Disconnect when the page unloads
+    dojo.addOnUnload(function()
+    {
+        cometd.disconnect();
+    });
+
+    var cometURL = location.protocol + "//" + location.host + "/ws-eventing-svc/cometd";
+    cometd.configure({
+        url: cometURL,
+        logLevel: 'debug'
+    });
+
+    cometd.addListener('/meta/connect', _metaConnect);
+
+    cometd.handshake();
+});
