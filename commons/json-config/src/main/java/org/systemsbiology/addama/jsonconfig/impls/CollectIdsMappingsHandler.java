ea6fd68c97417e58b1152d04af5987664d7e3231
Hector Rovira <hrovira.isb@gmail.com>
2011-12-28 16:04:19 -0800

    added impl to collect ids

---
+++ commons/json-config/src/main/java/org/systemsbiology/addama/jsonconfig/impls/CollectIdsMappingsHandler.java
@@ -0,0 +1,21 @@
+package org.systemsbiology.addama.jsonconfig.impls;
+
+import org.systemsbiology.addama.jsonconfig.Mapping;
+import org.systemsbiology.addama.jsonconfig.MappingsHandler;
+
+import java.util.Collection;
+
+/**
+ * @author hrovira
+ */
+public class CollectIdsMappingsHandler implements MappingsHandler {
+    private final Collection<String> ids;
+
+    public CollectIdsMappingsHandler(Collection<String> ids) {
+        this.ids = ids;
+    }
+
+    public void handle(Mapping mapping) throws Exception {
+        this.ids.add(mapping.id);
+    }
+}
