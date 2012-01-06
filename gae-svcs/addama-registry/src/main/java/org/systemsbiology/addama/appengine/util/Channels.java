ee872004684b6d6711276a9a2c9d448602647153
Hector Rovira <hrovira.isb@gmail.com>
2012-01-06 10:35:22 -0800

    extracted inner class

--- gae-svcs/addama-registry/src/main/java/org/systemsbiology/addama/appengine/util/Channels.java
+++ gae-svcs/addama-registry/src/main/java/org/systemsbiology/addama/appengine/util/Channels.java
@@ -2,19 +2,13 @@ package org.systemsbiology.addama.appengine.util;
 
 import com.google.appengine.api.channel.ChannelPresence;
 import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.memcache.Expiration;
 import com.google.appengine.api.memcache.MemcacheService;
+import org.systemsbiology.addama.appengine.callbacks.ChannelMemcacheLoaderCallback;
 import org.systemsbiology.addama.appengine.datastore.DeleteEntityTransactionCallback;
-import org.systemsbiology.addama.appengine.datastore.PutEntityTransactionCallback;
-import org.systemsbiology.addama.appengine.memcache.MemcacheLoaderCallback;
 
-import java.io.Serializable;
 import java.util.logging.Logger;
 
-import static com.google.appengine.api.channel.ChannelServiceFactory.getChannelService;
 import static com.google.appengine.api.datastore.DatastoreServiceFactory.getDatastoreService;
 import static com.google.appengine.api.datastore.KeyFactory.createKey;
 import static com.google.appengine.api.memcache.Expiration.byDeltaSeconds;
@@ -44,25 +38,4 @@ public class Channels {
         inTransaction(datastore, new DeleteEntityTransactionCallback(createKey("channels", clientId)));
     }
 
-    /*
-    * Private Classes
-    */
-    private static class ChannelMemcacheLoaderCallback implements MemcacheLoaderCallback {
-        public Serializable getCacheableObject(String userEmail) throws Exception {
-            Key k = createKey("channels", userEmail);
-            try {
-                Entity e = datastore.get(k);
-                return e.getProperty("token").toString();
-            } catch (EntityNotFoundException e) {
-                log.warning("token not found for " + userEmail + ":" + e.getMessage());
-            }
-
-            String token = getChannelService().createChannel(userEmail);
-
-            Entity e = new Entity(createKey("channels", userEmail));
-            e.setProperty("token", token);
-            inTransaction(datastore, new PutEntityTransactionCallback(e));
-            return token;
-        }
-    }
 }
