f07d10319eb07f617c44534ed0099404e0a1c564
Hector Rovira <hrovira.isb@gmail.com>
2012-01-06 15:49:51 -0800

    fixing registration

--- gae-svcs/addama-registry/src/main/java/org/systemsbiology/addama/appengine/rest/RegistryController.java
+++ gae-svcs/addama-registry/src/main/java/org/systemsbiology/addama/appengine/rest/RegistryController.java
@@ -5,22 +5,25 @@ import com.google.appengine.api.datastore.Entity;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.systemsbiology.addama.appengine.datastore.PutEntityTransactionCallback;
+import org.systemsbiology.addama.appengine.editors.JSONObjectPropertyEditor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import static com.google.appengine.api.datastore.DatastoreServiceFactory.getDatastoreService;
 import static java.util.UUID.randomUUID;
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static org.systemsbiology.addama.appengine.datastore.DatastoreServiceTemplate.inTransaction;
 import static org.systemsbiology.addama.appengine.util.Registry.*;
 import static org.systemsbiology.addama.appengine.util.Users.checkAdmin;
-import static org.systemsbiology.addama.appengine.datastore.DatastoreServiceTemplate.inTransaction;
 import static org.systemsbiology.addama.commons.web.utils.HttpIO.asPackage;
 
 /**
@@ -28,31 +31,37 @@ import static org.systemsbiology.addama.commons.web.utils.HttpIO.asPackage;
  */
 @Controller
 public class RegistryController {
+    private static final Logger log = Logger.getLogger(RegistryController.class.getName());
+
     private final DatastoreService datastore = getDatastoreService();
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(JSONObject.class, new JSONObjectPropertyEditor());
+    }
 
     @RequestMapping(value = "/registry", method = RequestMethod.POST)
     public void register(HttpServletRequest request, HttpServletResponse response,
-                         @RequestParam("registration") String registration) throws Exception {
+                         @RequestParam("registration") JSONObject registration) throws Exception {
         checkAdmin(request);
         try {
-            JSONObject json = new JSONObject(registration);
+            log.info(registration.toString());
 
             // TODO : Check ownership of services before proceeding
-
-            String serviceId = asPackage(new URL(json.getString("url")));
+            String serviceId = asPackage(new URL(registration.getString("url")));
             clearExistingService(serviceId);
 
-            ArrayList<Entity> entities = new ArrayList<Entity>();
+            // TODO : Parent/child relationships
 
             String registryKey = randomUUID().toString();
-            entities.add(newServiceEntity(serviceId, registryKey, json));
+            Entity pe = newServiceEntity(serviceId, registryKey, registration);
+            inTransaction(datastore, new PutEntityTransactionCallback(pe));
 
-            JSONArray mappings = json.getJSONArray("mappings");
+            JSONArray mappings = registration.getJSONArray("mappings");
             for (int i = 0; i < mappings.length(); i++) {
-                entities.add(newMappingEntity(serviceId, mappings.getJSONObject(i)));
+                Entity me = newMappingEntity(serviceId, mappings.getJSONObject(i));
+                inTransaction(datastore, new PutEntityTransactionCallback(me));
             }
-
-            inTransaction(datastore, new PutEntityTransactionCallback(entities));
 
             response.addHeader("x-addama-registry-key", registryKey);
         } catch (Exception e) {
