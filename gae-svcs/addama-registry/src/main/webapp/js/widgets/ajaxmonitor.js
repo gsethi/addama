adcdec1ac0203a6f47557cd797be6b0bf423df49
Hector Rovira <hrovira.isb@gmail.com>
2012-01-13 16:13:46 -0800

    fixed issues with ajax grid

--- gae-svcs/addama-registry/src/main/webapp/js/widgets/ajaxmonitor.js
+++ gae-svcs/addama-registry/src/main/webapp/js/widgets/ajaxmonitor.js
@@ -1,6 +1,9 @@
 Ext.ns("org.systemsbiology.addama.js");
 
 org.systemsbiology.addama.js.AjaxMonitor = Ext.extend(Object, {
+    SEQUENCE_ID: 1,
+    GRID_DATA: [],
+
     constructor: function(config) {
         Ext.apply(this, config);
 
@@ -11,33 +14,33 @@ org.systemsbiology.addama.js.AjaxMonitor = Ext.extend(Object, {
     },
 
     createGrid: function() {
-        var gridColumns = [
-            { header: "Sequence", width: 25, dataIndex: 'sequence', type: "int", sortable: true },
-            { header: "URI", width: 25, dataIndex: 'uri', sortable: true },
-            { header: "Date", width: 25, dataIndex: 'sentAt', type: "date", sortable: true, renderer: Ext.util.Format.dateRenderer() },
-            { header: "Status Code", width: 25, dataIndex: 'statusCode', sortable: true },
-            { header: "Status Text", width: 25, dataIndex: 'statusText' }
-        ];
-        var storeColumns = [
-            { name: "sequence", type: "int"},
-            { name: "uri" },
-            { name: "sentAt", type: "date" },
-            { name: "statusCode" },
-            { name: "statusText" },
-            { name: "responseText" }
-        ];
-
-        this.store = new Ext.data.ArrayStore({
+        this.store = new Ext.data.JsonStore({
             storeId: "store-ajax-monitor",
-            reader: new Ext.data.ArrayReader({}, storeColumns),
-            data: [],
-            sortInfo: {field: 'sequence', direction: "DESC"}
+            root: "responses",
+            idProperty: "id",
+            sortInfo: {field: "id", direction: "DESC"},
+            fields: [
+                { name: "id", type: "int"},
+                { name: "uri" },
+                { name: "sentAt", type: "date" },
+                { name: "statusCode" },
+                { name: "statusText" },
+                { name: "responseText" },
+                { name: "isJson", type: "boolean" }
+            ]
         });
 
-        var grid = new Ext.grid.GridPanel({
+        this.gridPanel = new Ext.grid.GridPanel({
             region: "center",
             store: this.store,
-            columns: gridColumns,
+            columns: [
+                { header: "ID", width: 50, dataIndex: 'id', type: "int", sortable: true, hidden: true },
+                { header: "URI", width: 200, dataIndex: 'uri', sortable: true },
+                { header: "Date", width: 100, dataIndex: 'sentAt', type: "date", sortable: true, renderer: Ext.util.Format.dateRenderer() },
+                { header: "Status Code", width: 75, dataIndex: 'statusCode', sortable: true },
+                { header: "Status Text", width: 100, dataIndex: 'statusText' },
+                { header: "Response Text", width: 300, dataIndex: 'responseText', hidden: true }
+            ],
             stripeRows: true,
             columnLines: true,
             frame:true,
@@ -46,39 +49,47 @@ org.systemsbiology.addama.js.AjaxMonitor = Ext.extend(Object, {
             animCollapse: false,
             iconCls: 'icon-grid'
         });
-        grid.on("rowclick", this.showResponseContent, this);
-
-        this.responseContent = new Ext.Panel({ title: "Response Content", region: "south" });
-
-        new Ext.Panel({
-            layout: "border",
-            items: [ grid, this.responseContent ]
-        });
+        this.gridPanel.on("rowclick", this.showResponseContent, this);
     },
 
-    addResponseToGrid: function(connection, response) {
-        this.store.add(new Ext.data.Record({
-            "id": this.incrementedId(),
-            "uri": connection.url,
-            "sentAt": new Date(),
-            "statusCode": response.status,
-            "statusText": response.statusText,
-            "responseText": response.responseText
-        }));
+    addResponseToGrid: function(connection, response, options) {
+        var contentType = response.getResponseHeader("Content-Type");
+        var isJson = (contentType != undefined && contentType.indexOf("ation/json") > 0);
+
+        var newData = {
+            id:this.incrementedId(),
+            uri: options.url,
+            sentAt: new Date(),
+            statusCode: response.status,
+            statusText: response.statusText,
+            responseText: response.responseText,
+            isJson: isJson
+        };
+        this.GRID_DATA.push(newData);
+        this.store.loadData({responses:this.GRID_DATA});
     },
 
     showResponseContent: function(g, rowIndex, e) {
-        this.responseContent.removeAll();
-        this.responseContent.add({html:"rowIndex=" + rowIndex});
+        var data = this.store.getAt(rowIndex).data;
+        var responseText = data.responseText;
+        if (data.isJson) {
+            var json = Ext.util.JSON.decode(responseText);
+            var stringify = JSON.stringify(json, null, "\u00a0\u00a0\u00a0\u00a0");
+            responseText = Ext.util.Format.nl2br(stringify);
+        }
+
+        new Ext.Window({
+            width:800,
+            height:400,
+            autoScroll: true,
+            modal: true,
+            title: "Response Content",
+            html: responseText
+        }).show();
     },
 
     incrementedId: function() {
-        if (!this.sequenceId) {
-            this.sequenceId = 1;
-        } else {
-            this.sequenceId++;
-        }
-        return this.sequenceId;
+        return this.SEQUENCE_ID++;
     }
 
 });
