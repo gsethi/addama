f0dc08f5336d1162ae64a3c81a304834cdaee876
Hector Rovira <hrovira.isb@gmail.com>
2012-01-13 17:20:21 -0800

    fixing workspaces

---
+++ gae-svcs/addama-registry/src/main/webapp/js/workspaces/tabPanels.js
@@ -0,0 +1,92 @@
+Ext.ns("org.systemsbiology.addama.js");
+
+org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
+    constructor: function(config) {
+        Ext.apply(this, config);
+
+        org.systemsbiology.addama.js.WorkspacesTabPanel.superclass.constructor.call(this);
+
+        this.loadPanels();
+    },
+
+    loadPanels: function() {
+        var startPanel = {
+            id: "main-content-start-panel",
+            layout: "fit",
+            bodyStyle: "padding:25px",
+            contentEl: "main-content-start"
+        };
+
+        var folderViewPanel = {
+            id: "main-content-folder-panel",
+            layout: "fit",
+            bodyStyle: "padding:25px",
+            contentEl: "main-content-folder"
+        };
+
+        var fileViewPanel = {
+            id: "main-content-file-panel",
+            layout: "fit",
+            bodyStyle: "padding:25px",
+            contentEl: "main-content-file"
+        };
+
+        var contentPanel = new Ext.Panel({
+            id: "panel-content-control",
+            title: "Main",
+            layout: "card",
+            activeItem: 0,
+            border: false,
+            items: [ startPanel, folderViewPanel, fileViewPanel ]
+        });
+
+        this.browsePanel = {
+            id: "layout-browser",
+            layout: "fit",
+            region:"west",
+            border: true,
+            split: true,
+            margins: "5 0 5 5",
+            width: 275,
+            minSize: 100,
+            maxSize: 500,
+            layoutConfig: {
+                titleCollapse: true,
+                hideCollapseTool: true,
+                animate: true,
+                activeOnTop: false
+            },
+            items: [tree]
+        };
+
+        var propertiesPanel = {
+            id: "panel-properties-control",
+            title: "Properties",
+            layout: "fit",
+            border:true,
+            margins: "5 5 5 0",
+            width: 320,
+            minSize: 100,
+            maxSize: 500,
+            animate: true,
+            activeOnTop: false,
+            bodyStyle: "padding-bottom:15px; background:#eee;",
+            autoScroll: true,
+            contentEl: "panel-properties"
+        };
+
+        this.mainPanel = new Ext.TabPanel({
+            id: "panel-main-control",
+            xtype: "tabpanel",
+            region: "center",
+            activeTab: 0,
+            margins: "5 5 5 0",
+            border: true,
+            split: true,
+            items:[ contentPanel, propertiesPanel],
+            bbar: statusBar
+        });
+    }
+
+});
+
