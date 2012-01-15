c14eaed9f95377eb4f9e8435dd79fb51403efd11
hrovira <hrovira@systemsbiology.org>
2012-01-14 22:04:01 -0800

    correcting workspace panels configuration

--- gae-svcs/addama-registry/src/main/webapp/js/workspaces/tabPanels.js
+++ gae-svcs/addama-registry/src/main/webapp/js/workspaces/tabPanels.js
@@ -8,78 +8,72 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
 
         org.systemsbiology.addama.js.WorkspacesTabPanel.superclass.constructor.call(this);
 
-        this.loadPanels();
-
         org.systemsbiology.addama.js.TreePanel = this;
+
+        this.loadPanels();
+        this.loadTree();
     },
 
     loadPanels: function() {
-        this.startPanel = {
+        this.startPanel = new Ext.Panel({
             id: "main-content-start-panel",
             layout: "fit",
             bodyStyle: "padding:25px",
             contentEl: "main-content-start"
-        };
+        });
 
-        this.folderViewPanel = {
+        this.folderViewPanel = new Ext.Panel({
             id: "main-content-folder-panel",
             layout: "fit",
             bodyStyle: "padding:25px",
             contentEl: "main-content-folder"
-        };
+        });
 
-        this.fileViewPanel = {
+        this.fileViewPanel = new Ext.Panel({
             id: "main-content-file-panel",
             layout: "fit",
             bodyStyle: "padding:25px",
             contentEl: "main-content-file"
-        };
+        });
 
         this.contentPanel = new Ext.Panel({
-                    id: "panel-content-control",
-                    title: "Main",
-                    layout: "card",
-                    activeItem: 0,
-                    border: false,
-                    items: [ this.startPanel, this.folderViewPanel, this.fileViewPanel ]
-                });
+            id: "panel-content-control",
+            title: "Main",
+            layout: "card",
+            region: "center",
+            activeItem: 0,
+            border: false,
+            items: [ this.startPanel, this.folderViewPanel, this.fileViewPanel ]
+        });
 
         this.treePanel = new Ext.tree.TreePanel({
-                    id: "tree-panel",
-                    title: "Workspaces",
-                    region:"north",
-                    split: true,
-                    minSize: 150,
-                    autoScroll: true,
-                    // tree-specific configs:
-                    rootVisible: false,
-                    lines: false,
-                    singleExpand: true,
-                    useArrows: true,
-                    loader: new Ext.tree.TreeLoader(),
-                    root: new Ext.tree.AsyncTreeNode()
-                });
-
-        this.browsePanel = {
-            id: "layout-browser",
-            layout: "fit",
+            id: "tree-panel",
+            title: "Workspaces",
             region:"west",
-            border: true,
             split: true,
+            minSize: 150,
+            autoScroll: true,
+            border: true,
             margins: "5 0 5 5",
             width: 275,
-            minSize: 100,
             maxSize: 500,
+            layout: "fit",
             layoutConfig: {
                 titleCollapse: true,
                 hideCollapseTool: true,
                 animate: true,
                 activeOnTop: false
             },
-            items: [this.treePanel]
-        };
+            // tree-specific configs:
+            rootVisible: false,
+            lines: false,
+            singleExpand: true,
+            useArrows: true,
+            loader: new Ext.tree.TreeLoader(),
+            root: new Ext.tree.AsyncTreeNode()
+        });
 
-        this.propertiesPanel = {
+        this.propertiesPanel = new Ext.Panel({
             id: "panel-properties-control",
             title: "Properties",
             layout: "fit",
@@ -93,25 +87,23 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
             bodyStyle: "padding-bottom:15px; background:#eee;",
             autoScroll: true,
             contentEl: "panel-properties"
-        };
+        });
 
-        this.mainPanel = new Ext.TabPanel({
-                    id: "panel-main-control",
-                    xtype: "tabpanel",
-                    region: "center",
-                    activeTab: 0,
-                    margins: "5 5 5 0",
-                    border: true,
-                    split: true,
-                    items:[ this.contentPanel, this.propertiesPanel],
-                    bbar: statusBar
-                });
+        this.mainPanel = new Ext.Panel({
+            id: "panel-main-control",
+            margins: "5 5 5 0",
+            layout: "border",
+            border: true,
+            split: true,
+            items:[ this.treePanel, this.contentPanel ],
+            bbar: statusBar
+        });
     },
 
     displayNodeInContentPanel: function(node) {
         var nodeCls = node.attributes.cls;
 
-        var layout = contentPanel.layout;
+        var layout = this.contentPanel.layout;
         if (layout) {
             if (nodeCls == "repository" || nodeCls == "folder") {
                 layout.setActiveItem("main-content-folder-panel");
@@ -131,12 +123,12 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
     displayNodeInPropertiesPanel: function (node) {
         Ext.getDom("panel-properties").innerHTML = "";
         var annotationsGrid = new Ext.grid.PropertyGrid({
-                    renderTo: "panel-properties",
-                    autoHeight: true,
-                    autoWidth: true,
-                    selModel: new Ext.grid.RowSelectionModel({singleSelect:true}),
-                    source: node.attributes
-                });
+            renderTo: "panel-properties",
+            autoHeight: true,
+            autoWidth: true,
+            selModel: new Ext.grid.RowSelectionModel({singleSelect:true}),
+            source: node.attributes
+        });
         annotationsGrid.render();
     },
 
@@ -156,11 +148,11 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
                 var repos = this.getJsonRepos(response);
 
                 this.treePanel.setRootNode(new Ext.tree.AsyncTreeNode({
-                            text: 'Workspaces',
-                            draggable:false,
-                            id:'addamatreetopid',
-                            children: repos.items
-                        }));
+                    text: 'Workspaces',
+                    draggable:false,
+                    id:'addamatreetopid',
+                    children: repos.items
+                }));
 
                 statusBar.displayMessage("Workspaces loaded");
             },
@@ -205,8 +197,7 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
             success: function(o) {
                 var json = Ext.util.JSON.decode(o.responseText);
                 if (json && json.items) {
-                    for (var i = 0; i < json.items.length; i++) {
-                        var item = json.items[i];
+                    Ext.each(json.items, function(item) {
                         if (!this.treePanel.getNodeById(item.uri)) {
                             item.text = item.label ? item.label : item.name;
                             item.id = item.uri;
@@ -221,7 +212,7 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
                             }
                             node.appendChild(item);
                         }
-                    }
+                    }, this);
                     node.renderChildren();
                 }
             },
@@ -241,7 +232,7 @@ org.systemsbiology.addama.js.WorkspacesTabPanel = Ext.extend(Object, {
 
         var mimeType = node.attributes.mimeType;
         if (mimeType && mimeType.substring(0, 5) == "image") {
-            Ext.getDom("main-content-file-preview").innerHTML = "<img src='" + uri + "' width='50%' heigth='50%'/>";
+            Ext.getDom("main-content-file-preview").innerHTML = "<img src='" + uri + "' width='50%' height='50%'/>";
         }
     }
 });
