cc98455131d1099558dedc5381227cfac00184a6
Hector Rovira <hrovira@systemsbiology.org>
2012-01-29 00:14:09 -0800

    improved jobs view

--- gae-svcs/addama-registry/src/main/webapp/js/jobsview.js
+++ gae-svcs/addama-registry/src/main/webapp/js/jobsview.js
@@ -29,208 +29,129 @@ org.systemsbiology.addama.js.widgets.jobs.JobBean = Ext.extend(Object, {
 
 });
 
-org.systemsbiology.addama.js.widgets.jobs.JobsGrid = Ext.extend(Ext.util.Observable, {
-    tools: [],
-    queue: [],
-    jobPointers: [],
-    gridData: [],
-    gridPanelConfig: {},
-
+org.systemsbiology.addama.js.widgets.jobs.View = Ext.extend(Object, {
     constructor: function(config) {
         Ext.apply(this, config);
 
-        this.addEvents({ queued: true, fetched: true });
+        org.systemsbiology.addama.js.widgets.jobs.View.superclass.constructor.call(this);
 
-        org.systemsbiology.addama.js.widgets.jobs.JobsGrid.superclass.constructor.call(this);
-
+        this.loadPanels();
+        this.loadTools();
         this.initListeners();
-        this.initRowExpander();
-        this.renderGrid();
-        this.fetchData();
+    },
 
-        this.refreshTask = { run: this.refetch, interval: 10000, scope: this };
+    loadPanels: function() {
+        this.toolsStore = new Ext.data.ArrayStore({ fields: [ "label", "uri" ], sortInfo: {field: "label"} });
+
+        this.listView = new Ext.list.ListView({
+            store: this.toolsStore,
+            region: "west",
+            width: 250,
+            frame:true,
+            border:true,
+            emptyText: "No tools found",
+            hideHeaders: true,
+            columns: [ { header: "Label", width: 300, sortable: true, dataIndex: "label" } ]
+        });
+        this.listView.on("click", this.loadJobs, this);
+
+        this.renderGrid();
+
+        this.mainPanel = new Ext.Panel({
+            layout: "border",
+            padding: "5 5 5 5",
+            margins: "5 5 5 5",
+            frame:true,
+            border:true,
+            items: [ this.listView, this.gridPanel ]
+        });
+    },
+
+    loadTools: function() {
+        Ext.Ajax.request({
+            url: "/addama/tools",
+            method: "GET",
+            success: function(o) {
+                var json = Ext.util.JSON.decode(o.responseText);
+                if (json && json.items) {
+                    this.displayTools(json.items);
+                }
+            },
+            failure: function(o) {
+                org.systemsbiology.addama.js.Message.error("Tools", "Error: " + o.responseText);
+            },
+            scope: this
+        })
+    },
+
+    loadJobs: function(view, index, node) {
+        var record = this.listView.getRecord(node);
+        Ext.Ajax.request({
+            url: record.data.uri + "/jobs",
+            method: "GET",
+            success: function(o) {
+                var json = Ext.util.JSON.decode(o.responseText);
+                if (json && json.items) {
+                    this.displayJobs(json.items);
+                }
+            },
+            failure: function(o) {
+                org.systemsbiology.addama.js.Message.error("Jobs", "Error: " + o.responseText);
+            },
+            scope: this
+        })
     },
 
     initListeners: function() {
-        this.on("queued", function() {
-            while (this.queue.length > 0) {
-                var toolUri = this.queue.shift();
-                if (toolUri) {
-                    Ext.Ajax.request({
-                        url: toolUri + "/jobs",
-                        method: "GET",
-                        success: function(o) {
-                            var json = Ext.util.JSON.decode(o.responseText);
-                            if (json && json.items) {
-                                Ext.each(json.items, this.addJob, this);
-                            }
-
-                            if (this.queue.length == 0) {
-                                this.fireEvent("fetched");
-                            }
-                        },
-                        scope: this
-                    });
-                }
-            }
-        }, this);
-
-        org.systemsbiology.addama.js.channels.Listener.on("message", function(a) {
-            var event = Ext.util.JSON.decode(a.data);
-            if (event.job) {
-                this.addJob(event.job);
-                this.refreshGrid();
-            }
-        }, this);
-
-        this.on("fetched", this.refreshGrid, this);
-    },
-
-    initRowExpander: function() {
-        this.rowExpander = new Ext.ux.grid.RowExpander({
-            tpl: new Ext.Template([
-                '<b>Results:</b>',
-                '<p>',
-                '<span id="container_details_results_{idx}"></span>'
-            ])
-        });
-        this.rowExpander.on("expand", function(rowexp, record) {
-            var jobUri = record.data.uri;
-            var containerEl = "container_details_results_" + record.data.idx;
-
-            Ext.Ajax.request({
-                url: jobUri,
-                method: "GET",
-                params: {
-                    "_dc": Ext.id()
-                },
-                success: function(o) {
-                    // track links
-                    var linkIds = [];
-                    var linkId = function() {
-                        var lid = Ext.id();
-                        linkIds[linkIds.length] = lid;
-                        return lid;
-                    };
-
-                    var ahrefs = [];
-
-                    var json = Ext.util.JSON.decode(o.responseText);
-                    if (json && json.items) {
-                        // build links
-                        Ext.each(json.items, function(item) {
-                            ahrefs[ahrefs.length] = "<a target='_blank' id='" + linkId() + "' href='" + item.uri + "'>" + item.name + "</a>";
-                        });
-                    } else {
-                        ahrefs[ahrefs.length] = "No results available.  Use 'Refresh' button to check again.";
-                    }
-
-                    ahrefs[ahrefs.length] = "<a target='_blank' id='" + linkId() + "' href='" + jobUri + "/log'>Job Log</a>";
-
-                    // render links
-                    var items = [];
-                    Ext.each(ahrefs, function(ahref) {
-                        items[items.length] = { html: ahref, frame: false, border: false };
-                    });
-
-                    new Ext.Panel({ items: items, renderTo: containerEl });
-
-                    // register event listener
-                    Ext.each(linkIds, function(lid) {
-                        var elem = Ext.get(lid);
-                        if (elem) {
-                            Ext.EventManager.on(elem, "click", trackDownloadLink);
+        if (org.systemsbiology.addama.js.channels && org.systemsbiology.addama.js.channels.Listener) {
+            org.systemsbiology.addama.js.channels.Listener.on("message", function(a) {
+                var event = Ext.util.JSON.decode(a.data);
+                if (event) {
+                    var job = event.job;
+                    if (job) {
+                        if (job.label) {
+                            org.systemsbiology.addama.js.Message.show("Job Status", job.label + ":" + job.status);
+                        } else {
+                            org.systemsbiology.addama.js.Message.show("Job Status", job.status + ":" + job.uri);
                         }
-                    });
+                    }
                 }
             });
+        }
+    },
 
-            return true;
-        }, this);
+    displayTools: function(items) {
+        if (items) {
+            var data = [];
+            Ext.each(items, function(item) {
+                data.push([item.label, item.uri]);
+            });
+            this.toolsStore.loadData(data);
+        }
+    },
+
+    displayJobs: function(items) {
+        if (items && items.length) {
+            var data = [];
+            Ext.each(items, function(item) {
+                data.push(this.addJob(item));
+            }, this);
+            this.jobsStore.loadData(data);
+        }
     },
 
     renderGrid: function() {
-        var selectionModel = new Ext.grid.CheckboxSelectionModel();
-        selectionMode.on("selectionchange", function(sm) {
-            if (sm.getCount()) {
-                this.grid.stopButton.enable();
-                this.grid.deleteButton.enable();
-            } else {
-                this.grid.stopButton.disable();
-                this.grid.deleteButton.disable();
-            }
+        this.detailsButton = new Ext.Button({ text: 'Show Details', iconCls:'show', disabled: true });
+        this.stopButton = new Ext.Button({ text: 'Stop Selected', iconCls:'stop', disabled: true });
+        this.deleteButton = new Ext.Button({ text: 'Delete Selected', iconCls:'delete', disabled: true });
+        var selectionModel = new Ext.grid.RowSelectionModel({singleSelect:true});
+
+        this.detailsButton.on("click", function() {
+            selectionModel.each(this.loadRowDetails, this);
         }, this);
 
-        var storeColumns = [
-            { name: 'idx' },
-            { name: 'uri' },
-            { name: 'tool' },
-            { name: 'lastChangeDay', type: 'date' },
-            { name: 'job' },
-            { name: 'owner' },
-            { name: 'status' },
-            { name: 'message' },
-            { name: 'reasonCode', type: 'int' },
-            { name: 'durationInSeconds', type: 'int' },
-            { name: 'lastModified', type: "date" }
-        ];
-
-        var gridColumns = [
-            this.rowExpander,
-            selectionMode,
-            { header: "Date", width: 25, dataIndex: 'lastChangeDay', type: "date", sortable: true, hidden: true },
-            { header: "URI", width: 25, dataIndex: 'uri', sortable: false, hidden: true },
-            { header: "Tool", width: 40, sortable: true, dataIndex: 'tool', id:'tool' },
-            { header: "Job", width: 40, sortable: true, dataIndex: 'job' },
-            { header: "Owner", width: 30, hidden: true, dataIndex: 'owner' },
-            { header: "Status", width: 20, sortable: true, dataIndex: 'status' },
-            { header: "Reason", width: 20, sortable: true, dataIndex: 'message' },
-            { header: "Code", width: 10, sortable: true, dataIndex: 'reasonCode' },
-            { header: "Duration (secs)", width: 20, sortable: true, dataIndex: 'durationInSeconds' },
-            { header: "Last Modified", width: 30, dataIndex: 'lastModified', type: "date", sortable: true, hidden: true }
-        ];
-
-        this.store = new Ext.data.GroupingStore({
-            reader: new Ext.data.ArrayReader({}, storeColumns),
-            data: this.gridData,
-            sortInfo: {field: 'lastModified', direction: "DESC"},
-            groupField:'lastChangeDay',
-            groupOnSort: false,
-            groupDir: "DESC"
-        });
-
-        var defaultConfig = {
-            store: this.store,
-            columns: gridColumns,
-            view: new Ext.grid.GroupingView({
-                forceFit:true,
-                startCollapsed: false,
-                groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "items" : "item"]})'
-            }),
-            sm: selectionMode,
-            tbar: [
-                { text: 'Refresh', iconCls:'refresh', ref: "../refreshButton" },
-                '-',
-                { text: 'Stop Selected', iconCls:'stop', disabled: true, ref: "../stopButton" },
-                '-',
-                { text: 'Delete Selected', iconCls:'delete', disabled: true, ref: "../deleteButton" },
-                '-',
-                { text: 'Auto-Refresh', enableToggle: true, toggleHandler: this.onAutoRefresh, pressed: false, scope: this }
-            ],
-            stripeRows: true,
-            columnLines: true,
-            frame:true,
-            plugins: this.rowExpander,
-            title: "Jobs",
-            collapsible: false,
-            animCollapse: false,
-            iconCls: 'icon-grid'
-        };
-
-        this.grid = new Ext.grid.GridPanel(Ext.apply(defaultConfig, this.gridPanelConfig));
-
-        this.grid.stopButton.on("click", function() {
-            selectionMode.each(function(row) {
+        this.stopButton.on("click", function() {
+            selectionModel.each(function(row) {
                 Ext.Ajax.request({
                     url: row.data.uri + "/stop",
                     method: "POST",
@@ -240,11 +161,10 @@ org.systemsbiology.addama.js.widgets.jobs.JobsGrid = Ext.extend(Ext.util.Observa
                     scope: this
                 });
             }, this);
-            selectionMode.clearSelections(true);
         }, this);
 
-        this.grid.deleteButton.on("click", function() {
-            selectionMode.each(function(row) {
+        this.deleteButton.on("click", function() {
+            selectionModel.each(function(row) {
                 Ext.Ajax.request({
                     url: row.data.uri + "/delete",
                     method: "POST",
@@ -259,69 +179,137 @@ org.systemsbiology.addama.js.widgets.jobs.JobsGrid = Ext.extend(Ext.util.Observa
             }, this);
         }, this);
 
-        this.grid.refreshButton.on("click", this.refetch, this);
+        selectionModel.on("selectionchange", function(sm) {
+            if (sm.getCount()) {
+                this.detailsButton.enable();
+                this.stopButton.enable();
+                this.deleteButton.enable();
+            } else {
+                this.detailsButton.disable();
+                this.stopButton.disable();
+                this.deleteButton.disable();
+            }
+        }, this);
+
+        this.jobsStore = new Ext.data.GroupingStore({
+            reader: new Ext.data.ArrayReader({}, [
+                { name: 'uri' },
+                { name: 'job' },
+                { name: 'status' },
+                { name: 'owner' },
+                { name: 'reasonCode', type: 'int' },
+                { name: 'message' },
+                { name: 'durationInSeconds', type: 'int' },
+                { name: 'created', type: 'date' },
+                { name: 'lastModified', type: "date" }
+            ]),
+            sortInfo: {field: 'lastModified', direction: "DESC"},
+            groupField:'status',
+            groupOnSort: false,
+            groupDir: "DESC"
+        });
+
+        var dtRender = Ext.util.Format.dateRenderer('m/d/Y H:i:s');
+
+        this.gridPanel = new Ext.grid.GridPanel({
+            region: "center",
+            store: this.jobsStore,
+            columns: [
+                { header: "Job", width: 40, sortable: true, dataIndex: 'job' },
+                { header: "Status", width: 20, sortable: true, dataIndex: 'status', hidden: true },
+                { header: "Code", width: 10, sortable: true, dataIndex: 'reasonCode' },
+                { header: "Reason", width: 20, sortable: true, dataIndex: 'message' },
+                { header: "Created", width: 25, dataIndex: 'created', type: "date", sortable: true, renderer: dtRender},
+                { header: "Last Modified", width: 30, dataIndex: 'lastModified', type: "date", sortable: true, renderer: dtRender},
+                { header: "Duration (secs)", width: 20, sortable: true, dataIndex: 'durationInSeconds' },
+                { header: "URI", width: 25, dataIndex: 'uri', sortable: false, hidden: true },
+                { header: "Owner", width: 30, dataIndex: 'owner', hidden: true }
+            ],
+            view: new Ext.grid.GroupingView({
+                forceFit:true,
+                startCollapsed: false,
+                groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "items" : "item"]})'
+            }),
+            sm: selectionModel,
+            tbar: [ this.detailsButton, '-', this.stopButton, '-', this.deleteButton ],
+            stripeRows: true,
+            columnLines: true,
+            frame:true,
+            title: "Jobs",
+            collapsible: false,
+            animCollapse: false,
+            iconCls: 'icon-grid'
+        });
     },
 
-    fetchData: function() {
+    loadRowDetails: function(row) {
+        var jobUri = row.data.uri;
+
         Ext.Ajax.request({
-            url: "/addama/tools",
+            url: jobUri,
             method: "GET",
             success: function(o) {
                 var json = Ext.util.JSON.decode(o.responseText);
                 if (json && json.items) {
-                    Ext.each(json.items, function(item) {
-                        this.tools[item.uri] = item;
-                        this.queue.push(item.uri);
-                    }, this);
-                    this.fireEvent("queued");
-
+                    this.displayRowDetails(json);
                 }
             },
             scope: this
         });
     },
 
-    refetch: function() {
-        this.jobPointers = [];
-        this.gridData = [];
-        this.refreshGrid();
-        this.fetchData();
-    },
+    displayRowDetails: function(json) {
+        // track links
+        var linkIds = [];
+        var linkId = function() {
+            var lid = Ext.id();
+            linkIds.push(lid);
+            return lid;
+        };
 
-    refreshGrid: function() {
-        this.store.loadData(this.gridData);
-    },
+        var jobBean = new org.systemsbiology.addama.js.widgets.jobs.JobBean(json);
 
-    onAutoRefresh: function(item, pressed) {
-        if (pressed) {
-            Ext.TaskMgr.start(this.refreshTask);
+        var results = [];
+        results.push({ html: "<b><a target='_blank' id='" + linkId() + "' href='" + json.uri + "/log'>Log</a></b>" });
+        if (json.items && json.items.length) {
+            var html = "";
+            Ext.each(json.items, function(item) {
+                html += "<li>" + "<a target='_blank' id='" + linkId() + "' href='" + item.uri + "'>" + item.name + "</a>" + "</li>"
+            });
+            results.push({ title: "Results", html: "<ul>" + html + "</ul>" });
         } else {
-            Ext.TaskMgr.stop(this.refreshTask);
+            results.push({ title: "Results", html: "No results found" });
         }
+
+        new Ext.Window({
+            title: "Job Results for " + jobBean.getLabel(),
+            closable: true,
+            modal: true,
+            closeAction: "hide",
+            autoScroll: true,
+            defaults: { frame: true, padding: "5 5 5 5", margins: "5 5 5 5", border: false },
+            items: results
+        }).show();
+
+        // listen to links
+        Ext.each(linkIds, function(lid) {
+            var elem = Ext.get(lid);
+            if (elem) elem.on("click", trackDownloadLink);
+        });
     },
 
     addJob: function(job) {
-        var tool = this.tools[job.script];
-        if (tool) {
-            var jobBean = new org.systemsbiology.addama.js.widgets.jobs.JobBean(job);
-            var jobPointer = this.jobPointers[job.uri];
-            if (!jobPointer) {
-                jobPointer = this.gridData.length;
-                this.jobPointers[job.uri] = jobPointer;
-            }
-            this.gridData[jobPointer] = [
-                jobPointer,
-                job.uri,
-                tool.label,
-                job.lastModified,
-                jobBean.getLabel(),
-                jobBean.getOwner(),
-                job.status,
-                job.message,
-                job.returnCode,
-                job.durationInSeconds,
-                job.lastModified
-            ];
-        }
+        var jobBean = new org.systemsbiology.addama.js.widgets.jobs.JobBean(job);
+        return [
+            job.uri,
+            jobBean.getLabel(),
+            job.status,
+            jobBean.getOwner(),
+            job.returnCode,
+            job.message,
+            job.durationInSeconds,
+            Date.parse(job.created),
+            Date.parse(job.lastModified)
+        ];
     }
 });
