381354b54e9d7d50593b12f19c357e48d5b21b0e
hrovira <hrovira@systemsbiology.org>
2012-06-13 21:51:28 +0000

    fixing ie bs

--- gae-svcs/addama-registry/src/main/webapp/js/utils.js
+++ gae-svcs/addama-registry/src/main/webapp/js/utils.js
@@ -1,4 +1,4 @@
-if (!console) {
+if (typeof console === 'undefined') {
   if (!window.console || !console.firebug) {
     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
     "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
